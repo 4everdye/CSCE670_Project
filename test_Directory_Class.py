0b68fa75930798476a83558b1376674eb705e8fc
4everdye <zhq9055@gmail.com>
2014-04-15 22:18:55 -0500

    test Directory_Class

---
+++ test_Directory_Class.py
@@ -0,0 +1,12 @@
+__author__ = 'Hanqing'
+from Directory_Class import Directory_Class
+
+
+def main():
+    test_class = Directory_Class()
+    test_class.search("Shihua Zheng")
+    test_class.print_result()
+
+
+if __name__ == "__main__":
+    main()
