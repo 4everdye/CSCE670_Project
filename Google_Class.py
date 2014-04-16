714cb25e72d97a432d49659c406a4968f877c3ef
4everdye <zhq9055@gmail.com>
2014-04-16 16:36:36 -0500

    Google_Class
    Search name using Google custom search api
    initialize object with key, id, number of results
    list search_res stores the results as lists, each list is [title, snippet, link]

---
+++ Google_Class.py
@@ -0,0 +1,28 @@
+__author__ = 'Hanqing'
+import requests
+import urllib
+
+
+class Google_Class:
+    my_key = ""
+    my_id = ""
+    url = ""
+    limit = None
+    search_res = []
+
+    def __init__(self, key, id, limit):
+        self.my_key = key
+        self.my_id = id
+        self.limit = limit
+
+    def search(self, query):
+        self.url = 'https://www.googleapis.com/customsearch/v1?key={}&cx={}&alt=json&q={}&num={}'.format(self.my_key, self.my_id,urllib.quote_plus(query), self.limit)
+        result = requests.get(self.url)
+        result_json = result.json()
+        for item in result_json['items']:
+            self.search_res.append([item['title'].encode('utf-8'), item['snippet'].encode('utf-8'), item['link'].encode('utf-8')])
+        return self.search_res
+
+    def print_result(self):
+        for item in self.search_res:
+            print item[0] + ': \n' + item[1] + '\n' + item[2] + '\n'
