9d28a60dc4645c59f8fa708bc0dfb0291b5c3fad
4everdye <zhq9055@gmail.com>
2014-04-16 19:49:40 -0500

    test Twitter_Class

---
+++ test_Twitter_Class.py
@@ -0,0 +1,17 @@
+__author__ = 'Hanqing'
+from Twitter_Class import Twitter_Class
+
+
+def main():
+    access_token = "323861641-2QMc92Bg6jV7dDUYPg3yvSJmhRCpEJncvq0rGZzn"
+    access_token_secret = "soso40fYcGP9G4pr9fYj6GjSLVV0ny50KDJwBgiOrDcPO"
+    consumer_key = "7G5CoXNtxP6XbfgTSyNzzA"
+    consumer_secret = "98Upsqkir4vz5PAsRjwSyrkKwDGSB2QB4rI6dNXnAk"
+    test_class = Twitter_Class(10)
+    query = "obama"
+    test_class.search(query)
+    test_class.print_result()
+
+
+if __name__ == '__main__':
+    main()
