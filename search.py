f76c63eb2192ae5435b8bbab5b8181e92613d26e
4everdye <zhq9055@gmail.com>
2014-04-24 16:14:35 -0500

    Update search.py

--- search.py
+++ search.py
@@ -11,7 +11,7 @@ search_page = '''
 <h1 style="font-size:40px"><center><abbr title="An Efficient People Search Engine">Uncover Anyone</abbr><center></center></h1>
 <div>
 <center>
-<form method="post" action="/test_3.py">
+<form method="post" action="/result.py">
 Query: <input type="Query" name="Query" required autocomplete="on" autofocus style="width:200px" value=""/>
 <input type="submit" value="Search">
 <br>
